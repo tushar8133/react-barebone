db680d4d3205b6f9f28fdd32eb14f5346a99c440
tushar8133 <tushar.ku@HCL.COM>
2020-03-11 17:40:12 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,25 @@
+'use strict';
+
+const e = React.createElement;
+
+class LikeButton extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { liked: false };
+  }
+
+  render() {
+    if (this.state.liked) {
+      return 'You liked this.';
+    }
+
+    return e(
+      'button',
+      { onClick: () => this.setState({ liked: true }) },
+      'Like'
+    );
+  }
+}
+React.createElement(LikeButton)
+const domContainer = document.querySelector('#root');
+ReactDOM.render(React.createElement('button',null,'hi'), document.querySelector('#root'));
